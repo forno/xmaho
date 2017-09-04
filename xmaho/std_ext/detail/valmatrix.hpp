2e588ef258dc3347fef6e159ffc742ec07df9f2c
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-09-04 23:18:38 +0900

    Add access and begin/end methods

---
+++ xmaho/std_ext/detail/valmatrix.hpp
@@ -0,0 +1,99 @@
+#ifndef XMAHO_STD_EXT_DETAIL_VALMATRIX_H
+#define XMAHO_STD_EXT_DETAIL_VALMATRIX_H
+/*
+BSD 2-Clause License
+
+Copyright (c) 2017, Doi Yusuke
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "../valmatrix.hpp"
+
+namespace xmaho
+{
+namespace std_ext
+{
+namespace detail
+{
+
+std::size_t size(std::pair<std::size_t, std::size_t> pos)
+{
+  return pos.first * pos.second;
+}
+
+std::size_t get_serial_index(
+  std::pair<std::size_t, std::size_t> pos,
+  std::size_t col_size)
+{
+  return pos.first * col_size + pos.second;
+}
+
+}
+}
+}
+
+template<typename T>
+xmaho::std_ext::valmatrix<T>::valmatrix(size_type row_size, size_type col_size)
+  : std::valarray<T>(row_size * col_size),
+    size_ {row_size, col_size}
+{
+}
+
+template<typename T>
+const T& xmaho::std_ext::valmatrix<T>::operator[](position_type position) const
+{
+  return std::valarray<T>::operator[](detail::get_serial_index(position, size_.second));
+}
+
+template<typename T>
+T& xmaho::std_ext::valmatrix<T>::operator[](position_type position)
+{
+  return std::valarray<T>::operator[](detail::get_serial_index(position, size_.second));
+}
+
+template<typename T>
+const T* xmaho::std_ext::valmatrix<T>::begin() const noexcept
+{
+  return &std::valarray<T>::operator[](0);
+}
+
+template<typename T>
+T* xmaho::std_ext::valmatrix<T>::begin() noexcept
+{
+  return &std::valarray<T>::operator[](0);
+}
+
+template<typename T>
+const T* xmaho::std_ext::valmatrix<T>::end() const noexcept
+{
+  return (&std::valarray<T>::operator[](size() - 1)) + 1;
+}
+
+template<typename T>
+T* xmaho::std_ext::valmatrix<T>::end() noexcept
+{
+  return (&std::valarray<T>::operator[](size() - 1)) + 1;
+}
+
+#endif
---
+++ xmaho/std_ext/valmatrix.hpp
@@ -0,0 +1,142 @@
+#ifndef XMAHO_STD_EXT_VALMATRIX_H
+#define XMAHO_STD_EXT_VALMATRIX_H
+/*
+BSD 2-Clause License
+
+Copyright (c) 2017, Doi Yusuke
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/**
+ * @file std_ext/valmatrix.hpp
+ * @brief The valmatrix composit valarray with dimention data.
+ */
+
+#include <cstddef>
+#include <utility>
+#include <valarray>
+
+namespace xmaho
+{
+namespace std_ext
+{
+
+/**
+ * @brief The adapter class for matrix valarray.
+ *
+ * valarray class are useful for multiple dimension vector.
+ * But it haven't dimension values.
+ * This class hold dimension value and present some API.
+ *
+ * @invariant size() == row_size() * col_size()
+ *
+ * @tparam T Value type of valarray.
+ */
+template<typename T>
+class valmatrix : std::valarray<T>
+{
+public:
+  using std::valarray<T>::value_type;
+  using size_type = std::size_t;
+  using position_type = std::pair<size_type, size_type>;
+
+  /**
+   * @brief Default constructor for empty valmatrix.
+   */
+  valmatrix() = default;
+
+  /**
+   * @brief Construct by matrix size.
+   *
+   * Create valarray that size is row size * column size.
+   *
+   * @param[in] row_size Row size.
+   * @param[in] col_size Column size.
+   */
+  valmatrix(size_type row_size, size_type col_size);
+
+  /**
+   * @brief Access by position.
+   *
+   * @pre position.first < ros_size()
+   * @pre position.second < col_size()
+   * @param position Access point.
+   * @return Const reference of access point.
+   */
+  const T& operator[](position_type position) const;
+
+  /**
+   * @brief Access by position.
+   *
+   * @pre position.first < ros_size()
+   * @pre position.second < col_size()
+   * @param position Access point.
+   * @return Const reference of access point.
+   */
+  T& operator[](position_type position);
+
+  /**
+   * @brief Get begin iterator.
+   *
+   * @return Const begin iterator.
+   */
+  const T* begin() const noexcept;
+
+  /**
+   * @brief Get begin iterator.
+   *
+   * @return Begin iterator.
+   */
+  T* begin() noexcept;
+
+  /**
+   * @brief Get end iterator.
+   *
+   * @return Const end iterator.
+   */
+  const T* end() const noexcept;
+
+  /**
+   * @brief Get end iterator.
+   *
+   * @return End iterator.
+   */
+  T* end() noexcept;
+
+  //! @brief Allow std::valarray access.
+  using std::valarray<T>::operator[];
+
+  //! @brief Get element count.
+  using std::valarray<T>::size;
+
+private:
+  position_type size_;
+};
+
+}
+}
+
+#include "detail/valmatrix.hpp"
+
+#endif
