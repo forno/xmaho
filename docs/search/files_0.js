26e7a6bd99c6e16befd5c22785a14e5a065cf593
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-11-26 01:54:08 +0900

    Make doc valarray (#7)
    * Add documents for valarray utility functions
    
    * Update docs for valarray utility functions

--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['valarray_2ehpp',['valarray.hpp',['../valarray_8hpp.html',1,'']]],
   ['valmatrix_2ehpp',['valmatrix.hpp',['../valmatrix_8hpp.html',1,'']]]
 ];
--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['row',['row',['../classxmaho_1_1std__ext_1_1valmatrix.html#a104c504100eab17084c72d47c912eadd',1,'xmaho::std_ext::valmatrix::row(std::size_t index) const '],['../classxmaho_1_1std__ext_1_1valmatrix.html#a254acb96fc5295f2b96551d42b11788c',1,'xmaho::std_ext::valmatrix::row(std::size_t index)']]],
-  ['row_5fsize',['row_size',['../classxmaho_1_1std__ext_1_1valmatrix.html#aba09b2444222786794df1ae3a5f4fdca',1,'xmaho::std_ext::valmatrix']]]
+  ['inner_5fproduct',['inner_product',['../valarray_8hpp.html#a4cac94ac200df2c129ca84967a3cba72',1,'xmaho::std_ext']]]
 ];
