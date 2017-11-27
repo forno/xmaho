b02a336e386d6dc5edf8a0e3ffd39e9014b8e144
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-11-27 15:31:37 +0900

    Add detail function as size error checker

--- xmaho/std_ext/detail/valmatrix.hpp
+++ xmaho/std_ext/detail/valmatrix.hpp
@@ -52,6 +52,11 @@ inline std::size_t get_serial_index(
   return pos.first * col_size + pos.second;
 }
 
+constexpr std::pair<std::size_t, std::size_t> get_matrix_size(std::size_t row_size, std::size_t col_size)
+{
+  return {col_size ? row_size : 0 , row_size ? col_size : 0};
+}
+
 }
 }
 }
@@ -59,28 +64,28 @@ inline std::size_t get_serial_index(
 template<typename T>
 xmaho::std_ext::valmatrix<T>::valmatrix(size_type row_size, size_type col_size)
   : std::valarray<T>(row_size * col_size),
-    size_ {col_size ? row_size : 0, row_size ? col_size : 0}
+    size_ {detail::get_matrix_size(row_size, col_size)}
 {
 }
 
 template<typename T>
 xmaho::std_ext::valmatrix<T>::valmatrix(const T& value, size_type row_size, size_type col_size)
   : std::valarray<T>(value, row_size * col_size),
-    size_ {col_size ? row_size : 0, row_size ? col_size : 0}
+    size_ {detail::get_matrix_size(row_size, col_size)}
 {
 }
 
 template<typename T>
 xmaho::std_ext::valmatrix<T>::valmatrix(const std::valarray<T>& values, size_type row_size, size_type col_size)
   : std::valarray<T>(values.size() == row_size * col_size ? values : std::valarray<T>(row_size * col_size)),
-    size_ {col_size ? row_size : 0, row_size ? col_size : 0}
+    size_ {detail::get_matrix_size(row_size, col_size)}
 {
 }
 
 template<typename T>
 xmaho::std_ext::valmatrix<T>::valmatrix(std::valarray<T>&& values, size_type row_size, size_type col_size)
   : std::valarray<T>(values.size() == row_size * col_size ? std::move(values) : std::valarray<T>(row_size * col_size)),
-    size_ {col_size ? row_size : 0, row_size ? col_size : 0}
+    size_ {detail::get_matrix_size(row_size, col_size)}
 {
 }
 
