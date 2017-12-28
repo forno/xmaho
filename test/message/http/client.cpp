a4a02a2a17f445d876972a199b56b21b4048eef0
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-12-29 01:34:25 +0900

    Add message/http/client test

---
+++ test/message/http/client.cpp
@@ -0,0 +1,52 @@
+/*
+BSD 2-Clause License
+
+Copyright (c) 2017, Doi Yusuke
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include "xmaho/message/http/client.hpp"
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+TEST(MessageHTTPTest, StandardConstruct)
+{
+  xmaho::message::http::Client{"GET", "/"};
+}
+
+TEST(MessageHTTPTest, NullConstruct)
+{
+  EXPECT_ANY_THROW((xmaho::message::http::Client{"", ""}));
+  EXPECT_ANY_THROW((xmaho::message::http::Client{"", "/"}));
+  EXPECT_ANY_THROW((xmaho::message::http::Client{"GET", ""}));
+}
+
+TEST(MessageHTTPTest, ToString)
+{
+  using namespace std::literals::string_literals;
+  EXPECT_EQ("GET /"s, static_cast<std::string>(xmaho::message::http::Client{"GET", "/"}));
+  EXPECT_EQ("GET / HTTP/1.1"s, static_cast<std::string>(xmaho::message::http::Client{"GET", "/", "HTTP/1.1"}));
+  EXPECT_EQ("GET / HTTP/1.1\r\n\r\n{id:1224}"s, static_cast<std::string>(xmaho::message::http::Client{"GET", "/", "HTTP/1.1", "{id:1224}"}));
+}
